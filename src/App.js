e29ddb12b238e5f5ad9ad388a91f72cec27c2b43
kdilot <kdilot@gmail.com>
2019-06-28 15:26:22 -0700

    default

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ExcelContainer } from 'container';
+
+function App() {
+  return (
+    <div>
+      <ExcelContainer />
+    </div>
+  );
+}
+
+export default App;
