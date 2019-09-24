0da2a665ee772a38058144114bf6ada936e98a1e
kdilot <kdilot@gmail.com>
2019-09-24 16:28:11 -0700

    component

---
+++ src/component/index.js
@@ -0,0 +1,5 @@
+export { default as FileLayout } from './FileLayout';
+export { default as JsonToCsv } from './JsonToCsv';
+export { default as JsonViewLayout } from './JsonViewLayout';
+export { default as TableLayout } from './TableLayout';
+export { default as Title } from './Title';
